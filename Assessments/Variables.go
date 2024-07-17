bcfab5482bb24772c71882232813ae49fb774f85
Madihaj14 <madihajk14@gmail.com>
2024-07-17 08:53:08 +0530

    Ex 3 added

--- Assessments/Variables.go
+++ Assessments/Variables.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
+
+	//EX1 (Print Function)
 	fmt.Println("Hey, I am beginning with Golang!")
 
+	//EX2 (Variable)
 	var smsSendingLimit int
 	var costPerSMS float64
 	var hasPermission bool
 	var username string
 	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+
+	//EX3 (Short Variable Declaration)
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+
+	fmt.Println(messageStart, age, messageEnd)
 }
