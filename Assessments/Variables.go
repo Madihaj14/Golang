e17a8fc5d375586246f022a03f6250b2a8360c77
Madihaj14 <madihajk14@gmail.com>
2024-07-18 08:24:22 +0530

    Updated new exercises

--- Assessments/Variables.go
+++ Assessments/Variables.go
@@ -11,8 +11,8 @@ func main() {
 	var smsSendingLimit int
 	var costPerSMS float64
 	var hasPermission bool
-	var username string
-	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+	var usernamee string
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, usernamee)
 
 	//EX3 (Short Variable Declaration)
 	messageStart := "Happy birthday! You are now"
@@ -31,4 +31,32 @@ func main() {
 	accountAgeInt := int(accountAgeFloat)
 
 	fmt.Println("Your account has existed for", accountAgeInt, "years")
+
+	//EX6 (Concatenation)
+	var username string = "presidentSkroob"
+	var password string = "12345"
+
+	fmt.Println("Authorization: Basic", username+":"+password)
+
+	//EX7 (Constants)
+	const premiumPlanName = "Premium Plan"
+	const basicPlanName = "Basic Plan"
+
+	fmt.Println("plan:", premiumPlanName)
+	fmt.Println("plan:", basicPlanName)
+
+	//EX 8 (Computed Constants)
+	const secondsInMinute = 60
+	const minutesInHour = 60
+	const secondsInHour = secondsInMinute * minutesInHour
+
+	fmt.Println("number of seconds in an hour:", secondsInHour)
+
+	//EX9 (Formatting Strings in Go)
+	const name = "Saul Goodman"
+	const openRate = 30.5
+
+	msg := fmt.Sprintf("Hi %s, your open rate is %.1f percent\n", name, openRate)
+
+	fmt.Print(msg)
 }
