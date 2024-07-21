35fcc02ef481bb5f3feed0bab6d129335e1e94ad
Madihaj14 <madihajk14@gmail.com>
2024-07-21 21:51:19 +0530

    updated

--- Functions/Code.go
+++ Functions/Code.go
@@ -17,34 +17,3 @@ func main() {
 func test(s1 string, s2 string) {
 	fmt.Println(concat(s1, s2))
 }
-
-
-package main
-
-import "fmt"
-
-func main() {
-
-    var a [5]int
-    fmt.Println("emp:", a)
-
-    a[4] = 100
-    fmt.Println("set:", a)
-    fmt.Println("get:", a[4])
-
-    fmt.Println("len:", len(a))
-
-    var twoD [2][3]int
-    for i := 0; i < 2; i++ {
-        for j := 0; j < 3; j++ {
-            twoD[i][j] = i + j
-        }
-    }
-    fmt.Println("2d: ", twoD)
-
-    twoD = [2][3]int{
-        {1, 2, 3},
-        {1, 2, 3},
-    }
-    fmt.Println("2d: ", twoD)
-}
