66bb732df925bc901747a7405d90e5ff15f59d43
Madihaj14 <madihajk14@gmail.com>
2024-07-21 20:45:10 +0530

    Update

--- Functions/Code.go
+++ Functions/Code.go
@@ -34,15 +34,6 @@ func main() {
 
     fmt.Println("len:", len(a))
 
-    b := [5]int{1, 2, 3, 4, 5}
-    fmt.Println("dcl:", b)
-
-    b = [...]int{1, 2, 3, 4, 5}
-    fmt.Println("dcl:", b)
-
-    b = [...]int{100, 3: 400, 500}
-    fmt.Println("idx:", b)
-
     var twoD [2][3]int
     for i := 0; i < 2; i++ {
         for j := 0; j < 3; j++ {
