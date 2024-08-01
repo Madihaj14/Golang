02dd7fc8f6ec0e8d7a9ba020e222254d38098bb3
Madihaj14 <madihajk14@gmail.com>
2024-08-01 18:01:36 +0530

    new exercise

--- Functions/Code.go
+++ Functions/Code.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//Ex 1 Functions
 func concat(s1 string, s2 string) string {
 	return s1 + s2
 }
@@ -14,4 +15,16 @@ func main() {
 
 func test(s1 string, s2 string) {
 	fmt.Println(concat(s1, s2))
+}
+
+//Ex2 Passing variables by value
+
+func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) int {
+	lastMonthBill := getBillForMonth(costPerSend, numLastMonth)
+	thisMonthBill := getBillForMonth(costPerSend, numThisMonth)
+	return thisMonthBill - lastMonthBill
+}
+
+func getBillForMonth(costPerSend, messagesSent int) int {
+	return costPerSend * messagesSent
 }
