93d621fbdbe904b7893597b6355619a903c1ce62
Madihaj14 <madihajk14@gmail.com>
2024-08-01 18:31:57 +0530

    updated

--- Functions/Code.go
+++ Functions/Code.go
@@ -11,20 +11,9 @@ func main() {
 	test("Lane,", " happy birthday!")
 	test("Elon,", " hope that Tesla thing works out")
 	test("Go", " is fantastic")
+
 }
 
 func test(s1 string, s2 string) {
 	fmt.Println(concat(s1, s2))
-}
-
-//Ex2 Passing variables by value
-
-func monthlyBillIncrease(costPerSend, numLastMonth, numThisMonth int) int {
-	lastMonthBill := getBillForMonth(costPerSend, numLastMonth)
-	thisMonthBill := getBillForMonth(costPerSend, numThisMonth)
-	return thisMonthBill - lastMonthBill
-}
-
-func getBillForMonth(costPerSend, messagesSent int) int {
-	return costPerSend * messagesSent
 }
