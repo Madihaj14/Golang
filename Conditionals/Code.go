3b9d0840d85ed24191209617f1252820ee667870
Madihaj14 <madihajk14@gmail.com>
2024-08-04 23:47:25 +0530

    new

--- Conditionals/Code.go
+++ Conditionals/Code.go
@@ -8,10 +8,4 @@ func main() {
 	maxMessageLen := 20
 	fmt.Println("Trying to send a message of length:", messageLen, "and a max length of:", maxMessageLen)
 
-	if messageLen <= maxMessageLen {
-		fmt.Println("Message sent")
-	} else {
-		fmt.Println("Message not sent")
-	}
-
 }
