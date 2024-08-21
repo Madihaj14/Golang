a3161d9113337be9877b384a7d068908085719d3
Madihaj14 <madihajk14@gmail.com>
2024-08-21 23:56:19 +0530

    Update

---
+++ Que3/Code.go
@@ -0,0 +1,41 @@
+//Find All Anagrams in a String
+
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func findAnagrams(s string, p string) []int {
+	var result []int
+	pCount := make(map[rune]int)
+	sCount := make(map[rune]int)
+
+	for _, char := range p {
+		pCount[char]++
+	}
+
+	for i, char := range s {
+		sCount[char]++
+		if i >= len(p) {
+			removeChar := rune(s[i-len(p)])
+			if sCount[removeChar] == 1 {
+				delete(sCount, removeChar)
+			} else {
+				sCount[removeChar]--
+			}
+		}
+		if reflect.DeepEqual(pCount, sCount) {
+			result = append(result, i-len(p)+1)
+		}
+	}
+
+}
+
+func main() {
+	s := "cbaebabacd"
+	p := "abc"
+	result := findAnagrams(s, p)
+	fmt.Println(result) // Output: [0 6]
+}
